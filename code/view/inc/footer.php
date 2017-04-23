55f5a3f5dd7082e0964d98040bde175e9ecd4e2c
Marton Kiss <kissmarc.att@gmail.com>
2017-04-23 17:38:29 +0100

    footer design elements added for a fresher and nicer look to the page

--- code/view/inc/footer.php
+++ code/view/inc/footer.php
@@ -1,7 +1,11 @@
 <hr>
 
 <footer>
-    <p>&copy; Social network &ndash; Kiss Márton Attila, Petrovszki Dániel</p>
+    <p>&copy; Közösségi háló &ndash; Petrovszki Dániel, Kiss Márton Attila
+        <br>Nem-konvencionális adatbázisok
+        <br>SZTE TTIK
+        <br>2017</br>
+    </p>
 </footer>
 </div> <!-- /container -->        <script src="assets///ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
